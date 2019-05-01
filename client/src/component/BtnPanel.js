fda7a834fe0bda8a38232da87549c1c6af4d3076
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2019-05-01 10:55:18 -0700

    Added button placeholders on BtnPanel

--- client/src/component/BtnPanel.js
+++ client/src/component/BtnPanel.js
@@ -8,7 +8,44 @@ class BtnPanel extends React.Component {
         return (
             <div className="BtnPanel">
                 {/* Rendered Component */}
-                <Button />
+                {/* 1st Row (AC +/- % /) */}
+                <div>
+                    <Button />
+                    <Button />
+                    <Button />
+                    <Button />
+                </div>
+
+                {/* 2nd Row (7 8 9 X) */}
+                <div>
+                    <Button />
+                    <Button />
+                    <Button />
+                    <Button />
+                </div>
+
+                {/* 3rd Row (4 5 6 -) */}
+                <div>
+                    <Button />
+                    <Button />
+                    <Button />
+                    <Button />
+                </div>
+
+                {/* 4th Row (1 2 3 +) */}
+                <div>
+                    <Button />
+                    <Button />
+                    <Button />
+                    <Button />
+                </div>
+                
+                {/* 5th Row (0 . =) */}
+                <div>
+                    <Button />
+                    <Button />
+                    <Button />
+                </div>
             </div>
         );
     }
