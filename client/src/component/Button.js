7d30c07e038ed3d7599d2843409f2a8925a6990a
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2019-04-30 17:40:51 -0700

    Added blank component for Buttons

---
+++ client/src/component/Button.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// static layout - blank component for Buttons
+
+class Button extends React.Component {
+    render() {
+        return (
+            <div className="Button">
+                Buttons
+            </div>
+        );
+    }
+}
+
+export default Button;
