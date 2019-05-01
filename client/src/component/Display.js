ce711b8fb38bbcf7c66878ac476186a35f417d7c
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2019-04-30 17:38:50 -0700

    Removed favicon logo; added blank display component

---
+++ client/src/component/Display.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// static layout - blank component for Display Screen
+
+class Display extends React.Component {
+    render() {
+        return (
+            <div className="Display">
+                Display Screen
+            </div>
+        );
+    }
+}
+
+export default Display;
