76bf140356dab46877b0adb184051eee9639452c
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2019-04-30 17:43:03 -0700

    Added blank component for BtnPanel

---
+++ client/src/component/BtnPanel.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// static layout - blank component for Btn Panel
+
+class BtnPanel extends React.Component {
+    render() {
+        return (
+            <div className="BtnPanel">
+                Btn Panel
+            </div>
+        );
+    }
+}
+
+export default BtnPanel;
